bc5e5b49bd6922b7c851f9ad7f28c37e9a7baf28
RIZEONFX <rizeon03@gmail.com>
2025-08-20 10:13:22 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#26A69A", /* green */
+        secondary: "#64748B", /* semiDark for paragraph */
+        light: "#FAFAFA", /* semiLight */
+        dark: "#38424a" /* dark for headinh */
+      },
+    },
+  },
+  plugins: ["prettier-plugin-tailwindcss"],
+}
+
